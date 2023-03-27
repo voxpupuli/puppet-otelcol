be38d601544897ad378a52b800b7b5dab0fa65cc
Lukas Plattner <lukas.plattner@apa.at>
2023-03-27 14:51:13 +0200

    Add some progress

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,37 +1,41 @@
 class otelcol (
-  String  $package_name                          = $otelcol::params::package_name,
-  String  $ensure                                = $otelcol::params::ensure,
-  String  $config_file                           = $otelcol::params::config_file,
-  String  $config_file_owner                     = $otelcol::params::config_file_owner,
-  String  $config_file_group                     = $otelcol::params::config_file_group,
-  Optional[Stdlib::Filemode] $config_file_mode   = $otelcol::params::config_file_mode,
-  String  $config_folder                         = $otelcol::params::config_folder,
-  Optional[Stdlib::Filemode] $config_folder_mode = $otelcol::params::config_folder_mode,
-  Hash    $receivers                             = $otelcol::params::receivers,
+  String  $package_name                          = 'otelcol-contrib',
+  String  $package_ensure                        = 'installed',
+  String  $config_file                           = '/etc/otelcol-contrib/config.yaml',
+  String  $config_file_owner                     = 'root',
+  String  $config_file_group                     = 'root',
+  Stdlib::Filemode $config_file_mode             = '0644',
+  # String  $config_folder                         = $otelcol::params::config_folder,
+  # Optional[Stdlib::Filemode] $config_folder_mode = $otelcol::params::config_folder_mode,
+  Hash    $receivers                             = {
+    'otlp': {
+      'protocols': {
+        'http': {},
+        'grpc': {},
+      }
+    }
+  },
   Hash    $processors                            = {},
-  Hash    $exporters                             = $otelcol::params::exporters,
-  Boolean $manage_service                        = $otelcol::params::manage_service,
-  Boolean $manage_repo                           = $otelcol::params::manage_repo,
-  Boolean $manage_archive                        = $otelcol::params::manage_archive,
-  Boolean $manage_user                           = $otelcol::params::manage_user,
-  Optional[String] $repo_location                = $otelcol::params::repo_location,
-  Optional[String] $archive_location             = $otelcol::params::archive_location,
-  Optional[String[1]] $archive_version           = $otelcol::params::archive_version,
-  Optional[String] $archive_install_dir          = $otelcol::params::archive_install_dir,
-  Boolean $purge_config_fragments                = $otelcol::params::purge_config_fragments,
-  String  $repo_type                             = $otelcol::params::repo_type,
-  String  $windows_package_url                   = $otelcol::params::windows_package_url,
-  Boolean $service_enable                        = $otelcol::params::service_enable,
-  String  $service_ensure                        = $otelcol::params::service_ensure,
-  Array   $install_options                       = $otelcol::params::install_options,
-) inherits otelcol::params {
-  $service_hasstatus = $otelcol::params::service_hasstatus
-  $service_restart   = $otelcol::params::service_restart
-
+  Hash    $exporters                             = {},
+  Hash    $pipelines                             = {},
+  Hash    $extensions                            = {},
+  Boolean $manage_service                        = true,
+  Boolean $manage_archive                        = false,
+  Boolean $manage_user                           = false,
+  Optional[String[1]] $archive_location          = 'https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v',
+  Optional[String[1]] $archive_version           = 0.74.0,
+  Optional[String[1]] $archive_install_dir       = '/tmp',
+  # Boolean $purge_config_fragments                = false,
+  Stdlib::Ensure::Service  $service_ensure       = 'running',
+  # Array   $install_options                       = '',
+) {
   contain otelcol::install
   contain otelcol::config
-  contain otelcol::service
+  if($manage_service) {
+    contain otelcol::service
+    Class['otelcol::config'] ~> Class['otelcol::service']
+    Class['otelcol::install'] ~> Class['otelcol::service']
+  }
 
-  Class['otelcol::install'] -> Class['otelcol::config'] ~> Class['otelcol::service']
-  Class['otelcol::install'] ~> Class['otelcol::service']
+  Class['otelcol::install'] -> Class['otelcol::config']
 }
--- manifests/install.pp
+++ manifests/install.pp
@@ -19,25 +19,12 @@ class otelcol::install {
     }
   }
 
-  if $facts['os']['family'] == 'windows' {
-    # required to install otelcol on windows
-    require chocolatey
-
-    # package install
-    package { $otelcol::package_name:
-      ensure          => $otelcol::ensure,
-      provider        => chocolatey,
-      source          => $otelcol::windows_package_url,
-      install_options => $otelcol::install_options,
-    }
-  } else {
-    if ! $otelcol::manage_archive {
-      ensure_packages([$otelcol::package_name],
-        {
-          ensure          => $otelcol::ensure,
-          install_options => $otelcol::install_options,
-        }
-      )
-    }
+  if ! $otelcol::manage_archive {
+    ensure_packages([$otelcol::package_name],
+      {
+        ensure          => $otelcol::package_ensure,
+        # install_options => $otelcol::install_options,
+      }
+    )
   }
 }
