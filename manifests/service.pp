950c3176574af83db092862124457a032f4ecd76
Lukas Plattner <lukas.plattner@apa.at>
2023-05-12 00:09:44 +0200

    Adopt service

---
+++ manifests/service.pp
@@ -0,0 +1,31 @@
+# @summary Manages the Otelcol service
+#
+# @example Make sure Otelcol is running
+#   include 'otelcol::service'
+# 
+# @example Disable otelcol service
+#  class { 'otelcol::service':
+#    ensure => stopped,
+#  }
+#
+# @param ensure 
+#   Ensure service status
+#
+# @api private
+class otelcol::service (
+  Stdlib::Ensure::Service $ensure            = $otelcol::service_ensure,
+) {
+  # include install
+  include otelcol::install
+
+  # systemd::dropin_file { 'otelcol_service':
+  #   unit     => 'otelcol.service',
+  #   content  => epp('otelcol/otelcol.dropin.epp'),
+  #   filename => 'otelcol_override.conf',
+  # }
+  # ~> 
+  service { 'otelcol':
+    ensure  => $ensure,
+    require => Package['otelcol'],
+  }
+}
