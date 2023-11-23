cab00918c5358c7a34fb29d73735f231608abe0b
Lukas Plattner <lukas.plattner@apa.at>
2023-11-23 21:38:33 +0100

    Update contib example

--- examples/contrib_installation.pp
+++ examples/contrib_installation.pp
@@ -1,34 +1,42 @@
+otelcol::receiver { 'otlp' :
+  config    => {
+    'protocols' => {
+      'grpc' => { 'endpoint' => 'localhost:4319' },
+      'http' => { 'endpoint' => 'localhost:4320' },
+    },
+  },
+  pipelines => ['metrics'],
+}
+
+Otelcol::Receiver { 'prometheus' :
+  config    => {
+    'config' => {
+      'scrape_configs' => [
+        {
+          'job_name'        => 'otel-collector',
+          'scrape_interval' => '10s',
+          'static_configs'  => [
+            { 'targets' => ['localhost:8889'] }
+          ],
+        },
+      ],
+    },
+  },
+  pipelines => ['metrics'],
+}
+
+Otelcol::Exporter { 'logging':
+  config    => { 'verbosity' => 'detailed' },
+  pipelines => ['metrics'],
+}
+
+Otelcol::Processor { 'batch':
+  config    => {},
+  pipelines => ['metrics'],
+}
+
 class { 'otelcol':
-  manage_archive       => true,
+  manage_archive => true,
   package_name         => 'otelcol-contrib',
   metrics_address_port => 8889,
-  receivers            => {
-    'otlp'       => {
-      'protocols' => {
-        'grpc' => { 'endpoint' => 'localhost:4319' },
-        'http' => { 'endpoint' => 'localhost:4320' },
-      },
-    },
-    'prometheus' => {
-      'config' => {
-        'scrape_configs' => [
-          {
-            'job_name'        => 'otel-collector',
-            'scrape_interval' => '10s',
-            'static_configs'  => [
-              { 'targets' => ['localhost:8889'] }
-            ],
-          },
-        ],
-      },
-    },
-  },
-  exporters            => { 'logging' => { 'verbosity' => 'detailed' } },
-  pipelines            => {
-    'metrics' => {
-      'receivers' => ['otlp', 'prometheus'],
-      'exporters' => ['logging'],
-    },
-  },
-  processors           => { 'batch' => {} },
 }
