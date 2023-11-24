f0125102e7fa876e6f3bb4fe4721e464f86da636
Lukas Plattner <lukas.plattner@apa.at>
2023-11-24 12:27:02 +0100

    Add Check for listening port

--- spec/acceptance/otelcol_contrib_spec.rb
+++ spec/acceptance/otelcol_contrib_spec.rb
@@ -8,8 +8,9 @@ describe 'Otelcol class with contrib' do
       it { is_expected.to be_enabled }
       it { is_expected.to be_running }
     end
+
     describe port(8889) do
-      it { should be_listening }
+      it { is_expected.to be_listening }
     end
   end
 end
